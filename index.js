c610ac573ff10c3a766c9b1c8f21d71333680855
stufromnorthvan <stumitchell@irsss.ca>
2022-09-30 00:47:38 -0700

    cleaning a few things up, deleting some useless code

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
 const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss.js');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-//   console.log('It worked! Returned IP:', ip);
-// });
-
-// fetchCoordsByIP('207.81.124.203', (error, coords) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-//   console.log('It worked! Returned coordinates:', coords);
-// });
-
-// fetchISSFlyOverTimes(latLong, (error, flyovers) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-//   console.log(`It worked! Returned flyover times for latitude: ${latLong.latitude} longitude: ${latLong.longitude}`, flyovers.response);
-// });
-
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
